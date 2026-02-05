29bcdeeedc3211ac13df4f8c6d5cfc92be7ad85c
john <72079267+john-s21@users.noreply.github.com>
2026-02-05 14:18:01 +0530

    changes in roles files

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -1,15 +1,11 @@
 locals {
-  # This fixes the "nasty formatting" issue by doing the naming logic here
   role_name = "${var.project_prefix}-${var.environment}-${var.role_name}"
 }
 
-# The Role Itself
+# 1. The Role
 resource "aws_iam_role" "this" {
   name                 = local.role_name
   assume_role_policy   = var.assume_role_policy
-  
-  # This fixes the "Permission Boundary" bug. 
-  # If you don't provide a boundary, it won't crash anymore.
   permissions_boundary = var.permissions_boundary_arn != "" ? var.permissions_boundary_arn : null
   
   tags = {
@@ -18,16 +14,16 @@ resource "aws_iam_role" "this" {
   }
 }
 
-# Attachment 1: Custom Policies (Like your SecretsManager policy)
+# 2. Attachment: Custom Policies (Using count to avoid "known after apply" errors)
 resource "aws_iam_role_policy_attachment" "custom_policies" {
-  for_each   = toset(var.custom_policy_arns)
+  count      = length(var.custom_policy_arns)
   role       = aws_iam_role.this.name
   policy_arn = var.custom_policy_arns[count.index]
 }
 
-# Attachment 2: Managed Policies (Like ReadOnlyAccess)
+# 3. Attachment: Managed Policies
 resource "aws_iam_role_policy_attachment" "managed_policies" {
-  for_each   = toset(var.managed_policy_arns)
+  count      = length(var.managed_policy_arns)
   role       = aws_iam_role.this.name
   policy_arn = var.managed_policy_arns[count.index]
 }
