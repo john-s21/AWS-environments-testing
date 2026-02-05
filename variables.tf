a1eb4a0e7215a8c142da998aa12336b2060943cd
john <72079267+john-s21@users.noreply.github.com>
2026-02-05 14:02:32 +0530

    changed the region in tfvars files

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  default     = "ap-south-1"
+}
+
+
+variable "aws_account_number" {
+  description = "902193012760"
+  type        = string
+}
+
+variable "project_prefix" {
+  description = "Testproject"
+  type        = string
+}
+
+variable "environment" {
+  description = "The deployment environment (dev, sit, prod)"
+  type        = string
+  validation {
+    condition     = contains(["dev", "sit", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, sit, prod."
+ }
+
+
