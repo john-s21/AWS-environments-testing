8141193396fc8a2398dfbd2b99b10949af8b946e
john <72079267+john-s21@users.noreply.github.com>
2026-02-06 10:45:57 +0530

    small changes

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -2,7 +2,6 @@ locals {
   role_name = "${var.project_prefix}-${var.environment}-${var.role_name}"
 }
 
-# 1. The Role
 resource "aws_iam_role" "this" {
   name                 = local.role_name
   assume_role_policy   = var.assume_role_policy
@@ -14,14 +13,12 @@ resource "aws_iam_role" "this" {
   }
 }
 
-# 2. Attachment: Custom Policies (Using count to avoid "known after apply" errors)
 resource "aws_iam_role_policy_attachment" "custom_policies" {
   count      = length(var.custom_policy_arns)
   role       = aws_iam_role.this.name
   policy_arn = var.custom_policy_arns[count.index]
 }
 
-# 3. Attachment: Managed Policies
 resource "aws_iam_role_policy_attachment" "managed_policies" {
   count      = length(var.managed_policy_arns)
   role       = aws_iam_role.this.name
